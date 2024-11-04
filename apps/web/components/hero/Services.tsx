757de0c1859845fa79e94d8653a4e23ffe5ce154
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-11-04 16:35:25 +0530

    update_margin

--- apps/web/components/hero/Services.tsx
+++ apps/web/components/hero/Services.tsx
@@ -15,7 +15,7 @@ type ServiceType = {
 export default function Services({services}: {services: ServiceType[]}) {
   const router = useRouter()
   return (
-     <div className="container mx-auto px-4 py-8">
+     <div className="container mx-auto px-4">
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {services.map((card, index) => (
           <Card key={index} className="flex flex-col overflow-hidden hover:shadow-lg transition-shadow duration-300">
