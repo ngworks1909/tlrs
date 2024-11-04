be31fe24206b09213fc1f6428e608db2ad6ccb68
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2024-11-04 16:27:58 +0530

    update_position

--- apps/web/app/help/page.tsx
+++ apps/web/app/help/page.tsx
@@ -3,17 +3,19 @@ import construction from '../../public/construction.svg'
 import Image from 'next/image'
 import Navbar from '@/components/navbar/Navbar'
 
-// 
-
 export default function page() {
   return (
-    <div className="min-h-screen bg-background">
-      <Navbar/>
-      <main className='mx-auto px-4 py-8'>
-        <div className='flex items-center justify-center h-full flex-col gap-10'>
-         <Image src={construction} className='h-[200px] sm:h-[300px] transition-all ease-linear' alt="Under Development" />
-         <span className='text-xl font-medium text-center'>We're sorry! Page is under development</span>
-       </div>
+    <div className="min-h-screen bg-background flex flex-col">
+      <Navbar />
+      <main className='flex-grow flex items-center justify-center px-4 py-8'>
+        <div className='flex flex-col items-center justify-center gap-10 text-center'>
+          <Image 
+            src={construction} 
+            className='h-[200px] sm:h-[300px] w-auto transition-all ease-linear' 
+            alt="Under Development" 
+          />
+          <span className='text-xl font-medium'>We're sorry! Page is under development</span>
+        </div>
       </main>
     </div>
   )
