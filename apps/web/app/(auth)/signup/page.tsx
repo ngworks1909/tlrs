1eae3c9a0099910040297a472a1aab47b88921f4
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-01-15 19:23:20 +0530

    live

--- apps/web/app/(auth)/signup/page.tsx
+++ apps/web/app/(auth)/signup/page.tsx
@@ -1,7 +1,14 @@
 import Auth from '@/components/auth/Auth'
+import { NEXT_AUTH_CONFIG } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(NEXT_AUTH_CONFIG);
+    if(session){
+      redirect("/")
+    }
   return (
     <Auth type='Signup'/>
   )
